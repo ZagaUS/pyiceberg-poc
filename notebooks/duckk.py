05ba6e06d2cec173f8672c4edde58fd514a9b5c9
Saro006 <saravanakumarsk8056@gmail.com>
2024-07-19 12:36:23 +0530

    consuming-trace ingestion

--- notebooks/duckk.py
+++
@@ -1,9 +0,0 @@
-import duckdb
-import pandas as pd
-import pyarrow.parquet as pq
-
-parquet_file = f'./city_data.parquet'
-filter_column = 'State'
-states = ['California', 'Michigan', 'New York']
-ds = pq.ParquetDataset(parquet_file, filters=[('state','in', states)])
-df = ds.read().to_pandas()
